9b93c5a9963b76893cd8788861843099d565142f
tomoyukikashiro <ksrtmyk@gmail.com>
2017-02-12 16:40:18 -0500

    feat(main): added main js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,28 @@
-import angular from 'angular';
+import constants from './constants';
+import pivotalTracker from './pivotaltracker';
+import localDb from './localdb';
 
-angular.module('app', []);
+angular
+  .module('app', [
+    pivotalTracker.name,
+    localDb.name
+  ])
+  .constant('appConstants', constants)
+  .config((appConstants, LovefieldProvider) => {
+    'ngInject';
+    let schemaBuilder = LovefieldProvider.create(appConstants.DB_NAME, appConstants.DB_VERSION);
+    schemaBuilder.createTable('first')
+      .addColumn('id', LovefieldProvider.Types.INTEGER)
+      .addColumn('name', LovefieldProvider.Types.STRING)
+      .addPrimaryKey(['id']);
+
+    schemaBuilder.connect().then(() => {
+      // let table = db.getSchema().table('first');
+      // let row = table.createRow({
+      //   id: 1,
+      //   name: 'blabla'
+      // });
+
+      // return db.insert.into(table).values([row]).exec();
+    });
+  });
---
+++ app/scripts/constants.js
@@ -0,0 +1,4 @@
+export default {
+  DB_NAME: 'pivotaltracker_dashbaord',
+  DB_VERSION: 1
+};
