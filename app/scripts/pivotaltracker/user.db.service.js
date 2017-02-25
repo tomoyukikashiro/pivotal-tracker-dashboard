ceedf859d321cace15c4ca9515502c98d7134af5
tomoyukikashiro <ksrtmyk@gmail.com>
2017-02-25 14:52:36 -0500

    feat(db): added method to get myself

--- app/scripts/pivotaltracker/user.db.service.js
+++ app/scripts/pivotaltracker/user.db.service.js
@@ -6,4 +6,15 @@ export default class PtUserDbService extends DbBase {
     'ngInject';
     super(moment, $q, Lovefield, 'user', PT_USER_SERVICE_SCHEMA);
   }
+  getMe() {
+    return this.select()
+      .then(promises => {
+        let db = promises[0];
+        let table = promises[1];
+        return db.select().from(table).where(table.api_token.isNotNull()).exec()
+          .then(results => {
+            return results[0];
+          });
+      });
+  }
 }
