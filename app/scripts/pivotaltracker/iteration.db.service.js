7fb6e130ba80fdf6673254f2b008ec99e02de156
tomoyukikashiro <ksrtmyk@gmail.com>
2017-02-26 17:30:31 -0500

    feat(DB): made iteration db query shorthand

--- app/scripts/pivotaltracker/iteration.db.service.js
+++ app/scripts/pivotaltracker/iteration.db.service.js
@@ -6,4 +6,26 @@ export default class PtIterationDbService extends DbBase {
     'ngInject';
     super(moment, $q, Lovefield, 'iteration', PT_ITERATION_SERVICE_SCHEMA);
   }
+
+  getCurrent() {
+    return this.select()
+      .then(results => {
+        let [db, table] = results;
+        return db
+          .select(table.num)
+          .from(table)
+          .orderBy(table.start, lf.Order.DESC)
+          .limit(1)
+          .exec()
+          .then(results => {
+            return results[0];
+          });
+      });
+  }
+
+  getStoryIdsInCurrent() {
+    return this.getCurrent().then(iteration => {
+      return iteration.story_ids;
+    });
+  }
 }
