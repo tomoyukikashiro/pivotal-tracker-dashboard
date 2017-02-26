ff252e67b1319989cd6203f093056be56c66959d
tomoyukikashiro <ksrtmyk@gmail.com>
2017-02-26 17:29:20 -0500

    feat(api): made making api url method

--- app/scripts/pivotaltracker/api_base.js
+++ app/scripts/pivotaltracker/api_base.js
@@ -2,8 +2,12 @@ import constants from '../constants';
 
 export default class ApiBase {
   constructor(path) {
-    this.apiUrl = constants.PIVOTAL_TRACKER_API_BASE_URL + path;
+    this.apiUrl = this.getApiUrl(path);
     this.fields = [];
+  }
+
+  getApiUrl(path) {
+    return constants.PIVOTAL_TRACKER_API_BASE_URL + path;
   }
 
   getFields() {
